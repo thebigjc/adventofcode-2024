612d01ca5a08341ee27493d2721f9f29bf673f05
Jordan Christensen <thebigjc@gmail.com>
2024-12-08 14:49:02 -0500

    use numpy for speed

--- day8.py
+++ day8.py
@@ -22,6 +22,8 @@ TEST_INPUT_2 = """T....#....
 ....#.....
 .........."""
 
+import numpy as np
+
 
 def parse_map(data):
     lines = data.strip("\n").split("\n")
@@ -34,8 +36,25 @@ def parse_map(data):
     return lines, antennas
 
 
+def find_collinear_points(p1, p2, max_y, max_x):
+    y1, x1 = p1
+    y2, x2 = p2
+
+    y_coords, x_coords = np.meshgrid(np.arange(max_y), np.arange(max_x), indexing="ij")
+    points = np.stack([y_coords, x_coords], axis=-1).reshape(-1, 2)
+
+    v1 = np.array([y2 - y1, x2 - x1, 0])
+    v2 = np.column_stack([points - np.array([y1, x1]), np.zeros(len(points))])
+
+    cross_products = np.cross(v1, v2)
+
+    collinear_mask = np.all(np.abs(cross_products) < 1e-10, axis=1)
+    collinear_points = points[collinear_mask]
+
+    return {tuple(point) for point in collinear_points}
+
+
 def find_antinodes(lines, antennas):
-    # Group antennas by frequency
     freq_dict = {}
     for f, y, x in antennas:
         freq_dict.setdefault(f, []).append((y, x))
@@ -51,9 +70,12 @@ def find_antinodes(lines, antennas):
             continue
 
         for i in range(n):
+            y1, x1 = coords[i]
+            p1 = (y1, x1)
+
             for j in range(i + 1, n):
-                y1, x1 = coords[i]
                 y2, x2 = coords[j]
+                p2 = (y2, x2)
 
                 C1y, C1x = (2 * y1 - y2), (2 * x1 - x2)
                 C2y, C2x = (2 * y2 - y1), (2 * x2 - x1)
@@ -63,15 +85,8 @@ def find_antinodes(lines, antennas):
                 if 0 <= C2y < max_y and 0 <= C2x < max_x:
                     antinode_set_1.add((C2y, C2x))
 
-                for y in range(max_y):
-                    for x in range(max_x):
-                        v1_x = x2 - x1
-                        v1_y = y2 - y1
-                        v2_x = x - x1
-                        v2_y = y - y1
-
-                        if v1_x * v2_y - v1_y * v2_x == 0:
-                            antinode_set_2.add((y, x))
+                collinear = find_collinear_points(p1, p2, max_y, max_x)
+                antinode_set_2.update(collinear)
 
     return antinode_set_1, antinode_set_2
 
